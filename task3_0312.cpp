abd211fba039e4571eda75062d1bda5c71dcfe63
Yaroslav Bohonos <102082030+Bgns@users.noreply.github.com>
2023-01-05 18:11:43 +0200

    Create task3_0312.cpp

---
+++ task3_0312.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    int stepin;
+    cout << "Введіть степінь, до якого треба піднести число 2: ";
+    cin >> stepin;
+    cout << pow(2, stepin) << endl;
+}
